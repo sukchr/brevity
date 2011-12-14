a819020db5d6c32c455e39bd337e164e6bf41d75
sukchr <sukchr@gmail.com>
2011-12-14 10:28:17 +0100

    Stream.ToBinary()

---
+++ src/sukchr.Tests/Stream/ToBinary.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable InconsistentNaming
+
+using NUnit.Framework;
+
+namespace sukchr.Tests.Stream
+{
+    [TestFixture]
+    public class ToBinary
+    {
+        [Test]
+        public void Test()
+        {
+            "sukchr.Tests.Data.data.xml".Open().ToBinary();
+        }
+    }
+}
+
+// ReSharper restore InconsistentNaming
