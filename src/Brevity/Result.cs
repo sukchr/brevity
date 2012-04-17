202d3a4acb3d1203c94644ecfaab7138256f5cd2
sukchr <sukchr@gmail.com>
2012-04-17 08:55:07 +0200

    Result class.

---
+++ src/Brevity/Result.cs
@@ -0,0 +1,29 @@
+﻿namespace Brevity
+{
+    /// <summary>
+    /// A simple class that represents the result of some operation or check.
+    /// </summary>
+    public class Result
+    {
+        /// <summary>
+        /// True if the operation/check was successfull. 
+        /// </summary>
+        public bool IsSuccess { get; set; }
+        
+        /// <summary>
+        /// A meaningful message to explain the <see cref="IsSuccess"/> status.
+        /// </summary>
+        public string Message { get; set; }
+
+
+        public static implicit operator Result(bool isSuccess)
+        {
+            return new Result { IsSuccess = isSuccess };
+        }
+
+        public static implicit operator bool(Result result)
+        {
+            return result.IsSuccess;
+        }
+    }
+}
