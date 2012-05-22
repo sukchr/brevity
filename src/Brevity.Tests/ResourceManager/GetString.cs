4f09ba14e82dd00f917c9b7848f70436cbe895b0
sukchr <sukchr@gmail.com>
2012-05-22 13:42:03 +0200

    ResourceManagerExtensions

---
+++ src/Brevity.Tests/ResourceManager/GetString.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable InconsistentNaming
+
+using NUnit.Framework;
+using Shouldly;
+
+namespace Brevity.Tests.ResourceManager
+{
+    [TestFixture]
+    public class GetString
+    {
+        [Test]
+        public void Getting_resource_that_exists_should_format_resource()
+        {
+            var message = Resource.ResourceManager.GetString("user_not_found", "foo");
+            message.ShouldBe("User with username foo not found.");
+        }
+
+        [Test]
+        public void Getting_resource_that_doesnt_exists_should_not_throw_exception()
+        {
+            var message = Resource.ResourceManager.GetString("foo", "foo");
+            message.ShouldBe(null);
+        }
+    }
+}
+
+// ReSharper restore InconsistentNaming
---
+++ src/Brevity/ResourceManagerExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace Brevity
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="ResourceManager"/>.
+    /// </summary>
+    public static class ResourceManagerExtensions
+    {
+        /// <summary>
+        /// Returns the value of the specified string resource, formatted with the given arguments, using the current UI culture. 
+        /// </summary>
+        /// <param name="resourceManager"></param>
+        /// <param name="name"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+         public static string GetString(this ResourceManager resourceManager, string name, params object[] args)
+        {
+            return GetString(resourceManager, name, CultureInfo.CurrentUICulture, args);
+        }
+
+        /// <summary>
+        /// Returns the value of the specified string resource, formatted with the given arguments, using the specified culture. 
+        /// </summary>
+        /// <param name="resourceManager"></param>
+        /// <param name="name"></param>
+        /// <param name="culture"> </param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string GetString(this ResourceManager resourceManager, string name, CultureInfo culture, params object[] args)
+         {
+             var text = resourceManager.GetString(name, culture);
+             return string.IsNullOrEmpty(text) ? text : text.FormatWith(args);
+         }
+    }
+}
