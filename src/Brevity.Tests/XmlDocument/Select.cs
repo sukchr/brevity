acd71a11ba0d4abcbf6d73b75c359fc579709e96
sukchr <sukchr@gmail.com>
2012-09-27 14:10:46 +0200

    Added some tests.

--- src/Brevity.Tests/XmlDocument/Select.cs
+++ src/Brevity.Tests/XmlDocument/Select.cs
@@ -19,6 +19,28 @@ namespace Brevity.Tests.XmlDocument
 			xml.Select("//saml:Assertion/@IssueInstant").ShouldBe("2012-07-16T12:33:01Z");
 			xml.Select("//ds:DigestValue").ShouldBe("0MBtzQPiCf5RfWLmLpMw7arF76M=");
 		}
+
+		[Test]
+		public void Non_existent_node_should_not_fail()
+		{
+			var xml = "Brevity.Tests.Data.saml-assertion.xml"
+				.OpenEmbeddedResource()
+				.ToXml()
+				.RegisterNamespaces();
+
+			xml.Select("//saml:FOO").ShouldBe(null);
+		}
+
+		[Test]
+		public void Non_existent_node_should_not_fail_2()
+		{
+			var xml = "Brevity.Tests.Data.saml-assertion.xml"
+				.OpenEmbeddedResource()
+				.ToXml()
+				.RegisterNamespaces();
+
+			xml.Select("//saml:Assertion/@IssueInstant").ShouldBe("2012-07-16T12:33:01Z");
+		}
 	}
 }
 
