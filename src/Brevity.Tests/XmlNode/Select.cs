ac4a5335ea4b0648a9edb1f34919c237adbb4879
sukchr <sukchr@gmail.com>
2013-06-19 09:29:31 +0200

    XmlNode.Select()

---
+++ src/Brevity.Tests/XmlNode/Select.cs
@@ -0,0 +1,33 @@
+﻿// ReSharper disable InconsistentNaming
+
+using NUnit.Framework;
+using Shouldly;
+
+namespace Brevity.Tests.XmlNode
+{
+	[TestFixture]
+	public class Select
+	{
+		private readonly System.Xml.XmlDocument _xml = @"<data>
+<foo>
+	<bar>hello</bar>
+</foo>
+</data>".ToXml();
+
+		[Test]
+		public void Test()
+		{
+			var node = _xml.SelectSingleNode("//foo");
+			node.Select("bar").ShouldBe("hello");
+		}
+
+		[Test]
+		public void Non_existent_node_should_not_fail()
+		{
+			var node = _xml.SelectSingleNode("//foo");
+			node.Select("noob").ShouldBe(null);
+		}
+	}
+}
+
+// ReSharper restore InconsistentNaming
---
+++ src/Brevity/XmlNodeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace Brevity
+{
+	/// <summary>
+	/// Extension methods for <see cref="XmlNode"/>.
+	/// </summary>
+	public static class XmlNodeExtensions
+	{
+		/// <summary>
+		/// Gets the <see cref="XmlNode.InnerText"/> of the node that matches the <paramref name="xpath"/>.
+		/// </summary>
+		public static string Select(this XmlNode xmlNode, string xpath)
+		{
+			if (xmlNode == null)
+				return null;
+
+			if (string.IsNullOrEmpty(xpath))
+				return null;
+
+			var node = xmlNode.SelectSingleNode(xpath);
+
+			if (node == null)
+				return null;
+
+			return node.InnerText;
+		}
+	}
+}
