8543650017dbcfb77eb9cb94a4e2700956542b29
sukchr <sukchr@gmail.com>
2014-09-04 12:58:18 +0200

    Dictionary<string,string>.ToString()

---
+++ src/Brevity.Tests/Dictionary/ToString.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+// ReSharper disable InconsistentNaming
+using NUnit.Framework;
+
+namespace Brevity.Tests.Dictionary
+{
+	[TestFixture]
+	public class ToString
+	{
+		[Test]
+		public void Test1()
+		{
+			var dictionary = new System.Collections.Generic.Dictionary<string, string>
+			{
+				{"foo", "a"},
+				{"bar", "b"},
+			};
+
+			dictionary.ToString("=", itemSeparator: "\n").ShouldBe("foo=a\nbar=b");
+		}
+
+		[Test]
+		public void Test2()
+		{
+			var dictionary = new System.Collections.Generic.Dictionary<string, string>
+			{
+				{"foo", "a"},
+				{"bar", "b"},
+			};
+
+			dictionary.ToString("=", "{", "}", ",").ShouldBe("{foo=a},{bar=b}");
+		}
+
+		[Test]
+		public void Test3()
+		{
+			var dictionary = new System.Collections.Generic.Dictionary<string, string>
+			{
+				{"foo", "a"},
+				{"bar", "b"},
+			};
+
+			dictionary.ToString("\": \"", "{\"", "\"}", ",\n").ShouldBe("{\"foo\": \"a\"},\n{\"bar\": \"b\"}");
+		}
+	}
+}
+
+// ReSharper restore InconsistentNaming
---
+++ src/Brevity/DictionaryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Brevity
+{
+	public static class DictionaryExtensions
+	{
+		public static string ToString(this Dictionary<string, string> dictionary, string keyValueSeparator, string itemPrefix = null, string itemPostfix = null, string itemSeparator = null)
+		{
+			if(dictionary == null)
+				return null;
+
+			if(dictionary.Count == 0)
+				return string.Empty;
+
+			var text = new StringBuilder();
+
+			foreach (var key in dictionary.Keys)
+			{
+				if(!string.IsNullOrEmpty(itemSeparator) && text.Length != 0)
+					text.Append(itemSeparator);
+
+				text.AppendFormat("{0}{1}{2}{3}{4}", itemPrefix, key, keyValueSeparator, dictionary[key], itemPostfix);
+			}
+
+			return text.ToString();
+		}
+	}
+}
