1c2d1339b7e04f49da07f134781a3a04c0b7cfdb
sukchr <sukchr@gmail.com>
2015-07-03 14:09:30 +0200

    FixedRecord

---
+++ src/Brevity/FixedRecord/FixedRecordAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Brevity.FixedRecord
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+	public class FixedRecordAttribute : Attribute
+	{
+	}
+}
